3c828d65a49ddce343707bdb77e274f46b5073c5
Iron <jjarevalog@ufpso.edu.co>
2024-10-05 22:08:36 +0200

    Envio-1

---
+++ Contenido/Main.java
@@ -0,0 +1,12 @@
+/*
+Contar Dígitos en un Número
+Escribe una función que reciba un número entero y cuente cuántos dígitos tiene.
+*/
+
+public class Main {
+    public static void main (String[] args) {
+        //Inicio algoritmo
+        
+        //Fin algoritmo
+    }
+}
